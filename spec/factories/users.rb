656e02d5d1e403ffa3eef6d77c069c2c915f426e
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-06 11:11:06 +0900

    factorybotを記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    person = Gimei.name
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.email}
+    password {Faker::Lorem.characters(min_alpha: 1, min_numeric: 1)}
+    family_name {person.last.kanji}
+    first_name {person.first.kanji}
+    family_name_kana{person.last.katakana}
+    first_name_kana{person.first.katakana}
+    dob {Faker::Date.backward}
+  end
+end
