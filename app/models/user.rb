95e801d50a364cd0349b12ab469aebd2e60b506a
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-07 10:02:52 +0900

    不足分バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  VALIDATE_PASSWORD_REGEX=/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{2,}\z/i.freeze
+  VALIDATE_KATAKANA_REGEX=/\p{katakana}/.freeze
+  validates :password ,format:{
+    with: VALIDATE_PASSWORD_REGEX,
+    message: 'は半角英数字混合で設定してください。'
+  }
   validates :nickname ,presence: true
   validates :family_name ,presence: true
   validates :first_name ,presence: true
-  validates :family_name_kana ,presence: true
-  validates :first_name_kana ,presence: true
+  validates :family_name_kana ,presence: true,format:{
+    with: VALIDATE_KATAKANA_REGEX,
+    message: 'はカタカナで設定してください。',
+    allow_blank: true
+  }
+  validates :first_name_kana ,presence: true,format:{
+    with: VALIDATE_KATAKANA_REGEX,
+    message: 'はカタカナで設定してください。',
+    allow_blank: true
+  }
   validates :dob ,presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   @user = FactoryBot.build(:user)
-  binding.pry
+
+  
 end
