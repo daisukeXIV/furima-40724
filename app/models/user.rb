870f693be8f243be4a89d2da69a844c994f635eb
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-10 14:21:27 +0900

    名字名前に正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   VALIDATE_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{2,}\z/i
   VALIDATE_KATAKANA_REGEX = /\A\p{katakana}\z/
+  VALIDATE_NAME_REGEX = /\A[\p{Hiragana}\p{katakana}\p{Han}]+\z/
   validates :password, format: {
     with: VALIDATE_PASSWORD_REGEX,
     message: 'は半角英数字混合で設定してください。'
   }
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
+  validates :family_name, presence: true, format: {
+    with: VALIDATE_NAME_REGEX,
+    message: 'は全角の漢字ひらがなカタカナで入力してください。',
+    allow_blank: true
+  }
+  validates :first_name, presence: true, format: {
+    with: VALIDATE_NAME_REGEX,
+    message: 'は全角の漢字ひらがなカタカナで入力してください。',
+    allow_blank: true
+  }
   validates :family_name_kana, presence: true, format: {
     with: VALIDATE_KATAKANA_REGEX,
     message: 'はカタカナで設定してください。',
