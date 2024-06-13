78d9920609b217278cd6a1d4686594144a0929da
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-13 11:48:32 +0900

    productのpriceバリデーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: { only_integer: ture, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :user
     # '---'が格納されているid1を除外する記述
     with_options numericality: { other_than: 1, message: "can't be blank" } do
