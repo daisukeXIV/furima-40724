1fa607ab6b09624ef91c5992896894334ac41b6f
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-13 10:21:34 +0900

    productのモデルにassociationを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
   belongs_to :prefecture
+  belongs_to :postage
 end
