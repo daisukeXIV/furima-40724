9d61afa622cd555b6d07371bfc039c8316936911
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-14 14:20:18 +0900

    render時にjsが動作しなかった問題を修正

--- app/javascript/listing.js
+++ app/javascript/listing.js
@@ -1,17 +1,27 @@
 window.addEventListener('turbo:load',function(){
   if(document.URL.match("/items/new")){
-    const price_field = document.getElementById('item-price');
-    const tax_field = document.getElementById('add-tax-price');
-    const profit_field = document.getElementById('profit');
-    const tax_rate = 0.1
-    price_field.addEventListener('change',function(){
-      let price = price_field.value
-      // tax計算処理
-      let tax = Math.floor(price * tax_rate)
-      tax_field.innerText = tax
-      // profit計算処理
-      let profit = Math.floor(price - tax)
-      profit_field.innerText = profit
-    })
+    tax_calc();
   }
 })
+// 入力エラー時のrenderで描画された時の処理
+window.addEventListener('turbo:render',function(){
+  if(document.URL.match("/items/new")){
+    tax_calc();
+  }
+})
+
+function tax_calc(){
+  let price_field = document.getElementById('item-price');
+  let tax_field = document.getElementById('add-tax-price');
+  let profit_field = document.getElementById('profit');
+  const tax_rate = 0.1
+  price_field.addEventListener('change',function(){
+    let price = price_field.value
+    // tax計算処理
+    let tax = Math.floor(price * tax_rate)
+    tax_field.innerText = tax
+    // profit計算処理
+    let profit = Math.floor(price - tax)
+    profit_field.innerText = profit
+  })
+}
