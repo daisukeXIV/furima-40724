39098e59791dff2e0b09559539249957ba3d84ad
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-14 11:58:38 +0900

    jsが画面遷移時に動作しない問題の修正

--- app/javascript/listing.js
+++ app/javascript/listing.js
@@ -1,16 +1,17 @@
-window.addEventListener('load',function(){
-  const price_field = document.getElementById('item-price');
-  const tax_field = document.getElementById('add-tax-price');
-  const profit_field = document.getElementById('profit');
-  const tax_rate = 0.1
-  price_field.addEventListener('change',function(){
-    let price = price_field.value
-    
-    let tax = Math.floor(price * tax_rate)
-    tax_field.innerText = tax
-
-    let profit = Math.floor(price - tax)
-    profit_field.innerText = profit
-  })
-  
+window.addEventListener('turbo:load',function(){
+  if(document.URL.match("/items/new")){
+    const price_field = document.getElementById('item-price');
+    const tax_field = document.getElementById('add-tax-price');
+    const profit_field = document.getElementById('profit');
+    const tax_rate = 0.1
+    price_field.addEventListener('change',function(){
+      let price = price_field.value
+      // tax計算処理
+      let tax = Math.floor(price * tax_rate)
+      tax_field.innerText = tax
+      // profit計算処理
+      let profit = Math.floor(price - tax)
+      profit_field.innerText = profit
+    })
+  }
 })
