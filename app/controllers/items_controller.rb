3ed62ac4242bb6602431d9977c651fc307bc6226
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-20 13:11:02 +0900

    複数回使われている記述をメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_product, only: [:show, :edit]
   def index
     @products = Product.all.order('id DESC')
   end
@@ -9,13 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     return if current_user.id == @product.user_id
 
     redirect_to root_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
