52999e23624882d1fb5e864d27c184b8a5909bdb
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-20 13:28:16 +0900

    ルーティングの設定destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"items#index"
   resources :items, only:[:new,:create,:show,:edit]
-  resources :products, only:[:create,:update]
+  resources :products, only:[:create,:update,:destroy]
 end
