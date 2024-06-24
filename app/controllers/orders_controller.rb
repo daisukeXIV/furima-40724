e37b8ca15301300c9b2c0d6afc4b16bc98f4d8ab
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-24 13:14:46 +0900

    orders_controllerに保存の為に必要な記述を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,23 @@
 class OrdersController < ApplicationController
   def index
-    @product = Product.find(params[:format])
-    @address = Address.new
+    @product = Product.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_param)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      @product = Product.find(params[:item_id])
+      render 'orders/index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_param
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell).merge(user_id: current_user.id, product_id: params[:item_id])
   end
 end
