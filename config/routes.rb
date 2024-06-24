ae5a726b46493aef3ac89faab214b92d1421ab38
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-24 13:09:32 +0900

    出品機能実装に伴うルーティングの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"items#index"
-  resources :items, only:[:new,:create,:show,:edit]
-  resources :products, only:[:create,:update,:destroy]
-  resources :orders, only:[:index,:create]
+  resources :items, only:[:new,:create,:show,:edit]do
+    resources :orders, only:[:index,:create]
+  end
+  resources :products, only:[:create,:update,:destroy] 
 end
