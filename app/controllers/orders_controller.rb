7c5f442a19107cc7e44917b55467c951bc96c586
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-25 10:40:18 +0900

    order_controllerでの在庫切れ商品のアクセス制限とリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_product, only: [:index, :create]
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @product = Product.find(params[:item_id])
+    set_gon_publickey
     @order_address = OrderAddress.new
   end
 
   def create
-    @product = Product.find(params[:item_id])
     @order_address = OrderAddress.new(order_param)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      set_gon_publickey
       render 'orders/index', status: :unprocessable_entity
     end
   end
@@ -21,5 +20,17 @@ class OrdersController < ApplicationController
 
   def order_param
     params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell).merge(user_id: current_user.id, product_id: params[:item_id],token: params[:token], price: @product.price)
+  end
+
+  def set_product
+    @product = Product.find(params[:item_id])
+  end
+
+  def soldout_judge
+    set_product
+    redirect_to root_path unless @product.order.nil?
+  end
+  def set_gon_publickey
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 end
