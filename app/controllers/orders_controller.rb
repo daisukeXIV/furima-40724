1154337717754e1c94407d1701f067157036deb7
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-25 10:22:35 +0900

    クレジットカード決済の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,18 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @product = Product.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
+    @product = Product.find(params[:item_id])
     @order_address = OrderAddress.new(order_param)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      @product = Product.find(params[:item_id])
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'orders/index', status: :unprocessable_entity
     end
   end
@@ -18,6 +20,6 @@ class OrdersController < ApplicationController
   private
 
   def order_param
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell).merge(user_id: current_user.id, product_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell).merge(user_id: current_user.id, product_id: params[:item_id],token: params[:token], price: @product.price)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell, :user_id, :product_id)
+  attr_accessor(:post_code, :prefecture_id, :municipality, :street_address, :building, :tell, :user_id, :product_id, :token, :price)
 
   VALIDATE_POSTCODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
   VALIDATE_TELL_REGEX = /\A[0-9]{10,11}\z/
@@ -12,10 +12,24 @@ class OrderAddress
     validates :municipality
     validates :street_address
     validates :tell, format: { with: VALIDATE_TELL_REGEX, message: "は10桁以上11桁以内の半角数値で入力してください", allow_blank: true }
+    validates :token
+    balidates :price
   end
 
   def save
+    pay_item
     order = Order.create(user_id: user_id, product_id: product_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, street_address: street_address, building: building, tell: tell, order_id: order.id)
+  end
+
+  private
+
+  def pay_item
+    Payjp.api_key = 'ENV["PAYJP_SECRET_KEY"]'
+    Payjp::Charge.create(
+      amount: price,  # 商品の値段
+      card: token,    # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
