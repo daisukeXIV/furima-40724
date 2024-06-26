3fdcec2b03d07fccaf0e3323f7af5e9e35dc5c79
daisukeXIV <daisuke1998gamer@gmail.com>
2024-06-26 10:06:51 +0900

    商品購入機能単体テストコード

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    address = Gimei.address
+    tell_num_coount=Faker::Number.between(from: 10, to:11)
+    post_code { Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    municipality { address.city.kanji }
+    street_address { Faker::Address.building_number }
+    building { Faker::Company.name }
+    tell { Faker::Number.leading_zero_number(digits: tell_num_coount) }
+    token { Faker::Alphanumeric.alphanumeric(number: 30) }
+  end
+end
